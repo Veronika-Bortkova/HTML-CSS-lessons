c6e780a9ef2998ca05df66170c8bc0239cfbf437
Veronika-Bortkova <shishiga79@gmail.com>
2026-01-26 21:40:43 +0200

    HW 9 start

--- JS/Chapter-9/main.js
+++ JS/Chapter-9/main.js
@@ -37,23 +37,141 @@
 
 //---------------------3
 // – Є масив
-//
-// let coursesAndDurationArray = [
-//
-//     {title: ‘JavaScript Complex’, monthDuration: 5},
-//
-// {title: ‘Java Complex’, monthDuration: 6},
-//
-// {title: ‘Python Complex’, monthDuration: 6},
-//
-// {title: ‘QA Complex’, monthDuration: 4},
-//
-// {title: ‘FullStack’, monthDuration: 7},
-//
-// {title: ‘Frontend’, monthDuration: 4}
-//
-// ];
-//
 // Для кожного елементу масиву зробити блок, в якому вивести інформацію про title та monthDuration
 //
 // Завдання робити через цикли.
+// let coursesAndDurationArray = [
+//     {title: "JavaScript Complex", monthDuration: 5},
+//
+// {title: "Java Complex", monthDuration: 6},
+//
+// {title: "Python Complex", monthDuration: 6},
+//
+// {title: "QA Complex", monthDuration: 4},
+//
+// {title: "FullStack", monthDuration: 7},
+//
+// {title: "Frontend", monthDuration: 4}
+//
+// ];
+//
+// for (const element of coursesAndDurationArray) {
+//     let div = document.createElement("div");
+//     div.innerText = `Назва курсу - ${element.title} длительность курса - ${element.monthDuration}`;
+//     document.body.append(div);
+// }
+
+
+//-------------------------------------4
+
+// – Є масив
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class=’item’> ,  в якому буде <h1 class=’heading’>  з title  елементу, та <p class=’description’> з monthDuration елементу.
+//
+//     Завдання робити через цикли.
+
+//
+
+//--------------------------------5
+// – Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+//
+// Приклад структури знаходиться у файлі example.png, який лежить в папці з поточним файлом
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+
+for (const element of coursesArray) {
+    let divCourse = document.createElement("div");
+    divCourse.classList.add("course");
+    let ulModules = document.createElement("ul");
+    let i = 0;
+    for (const key in element) {
+        let divInfoCourse = document.createElement("div");
+        i++;
+        divInfoCourse.classList.add("courseInfo");
+        divInfoCourse.innerText = element[key];
+        if (i>1) {// роблю для того, щоб не було в верхнему блоку запису такого виду "title -  JavaScript Complex", бо виглядає якось не полюдськи. А в інших блоках, щоб навпаки було - бо незрозуміло без цього, яка інфа в них лежить
+            divInfoCourse.innerText = `${key} - ${element[key]}`;
+        }
+        divCourse.append(divInfoCourse);
+        if (Array.isArray(element[key])){
+            divInfoCourse.innerText = "Modules";
+            divInfoCourse.append(ulModules);
+        }
+    }
+         for (const ModulElement of element.modules) {
+             let liNameModul = document.createElement("li");
+             liNameModul.innerText = ModulElement;
+             ulModules.append(liNameModul);
+         }
+    document.body.append(divCourse);
+}
